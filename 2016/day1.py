d23e1b6d080d7c1dde6dd4beb9c87ed6c7f6640f
kristian@purestyle.se <kristian@purestyle.se>
2016-12-01 12:37:55 +0100

    day1

---
+++ 2016/day1.py
@@ -0,0 +1,45 @@
+instructions = [line.rstrip("\n") for line in open("input.txt", "r")]
+
+instr_ls = instructions[0].split(", ")
+
+cur_dir = "north";
+dist = (0,0)
+
+def new_dir(current, dir_mod):
+    if current == "north" and dir_mod == "L":
+        return "west"
+    elif current == "west" and dir_mod == "L":
+        return "south"
+    elif current == "south" and dir_mod == "L":
+        return "east"
+    elif current == "east" and dir_mod == "L":
+        return "north"
+    elif current == "north" and dir_mod == "R":
+        return "east"
+    elif current == "east" and dir_mod == "R":
+        return "south"
+    elif current == "south" and dir_mod == "R":
+        return "west"
+    elif current == "west" and dir_mod == "R":
+        return "north"
+
+for instr in instr_ls:
+    dir_mod = instr[0]
+    steps = int(instr[1:])
+    cur_dir = new_dir(cur_dir, dir_mod)
+    (new_x, new_y) = dist
+    if cur_dir == "north":
+        new_y = new_y - steps
+    elif cur_dir == "south":
+        new_y = new_y + steps
+    elif cur_dir == "west":
+        new_x = new_x - steps
+    elif cur_dir == "east":
+        new_x = new_x + steps
+    dist = (new_x, new_y)
+
+(final_x, final_y) = dist
+
+final = abs(final_x) + abs(final_y)
+
+print "Final: ", dist, final
