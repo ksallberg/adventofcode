17af8f4df7dc65eaa3993ad45933afe88aa5795f
kristian@purestyle.se <kristian@purestyle.se>
2016-12-06 16:56:39 +0100

    more

--- 2016/Day6.hs
+++ 2016/Day6.hs
@@ -5,9 +5,8 @@ import Data.List
 main :: IO ()
 main = do
   f <- readFile "input"
-  let x = map freq $ transpose $ lines f
-      w = map snd $ map (head . reverse . sort) x
-  putStrLn $ concat w
+  let x = map snd $ map (head . reverse . sort) (map freq $ transpose $ lines f)
+  putStrLn $ concat x
 
 freq :: String -> [(Int, String)]
 freq = map (\x->(length x, [head x])) . group . sort
