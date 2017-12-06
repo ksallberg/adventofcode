c6060292d48be77b6cf4c997882afdc462ac71bd
kristian sallberg <kristian@purestyle.se>
2017-12-06 21:58:50 +0100

    dag4 del1

---
+++ 2017/day4.lisp
@@ -0,0 +1,25 @@
+(defun run ()
+  (list-length (countit (get-file "input.txt"))))
+
+(defun countit (input)
+  (remove nil (transform input)))
+
+(defun transform (input)
+  (mapcar 'isok (mapcar 'str-split input)))
+
+(defun isok (strings)
+  (eq (remove-duplicates strings :test #'equal) strings))
+
+;; Utils
+;; https://rosettacode.org/wiki/Tokenize_a_string#Common_Lisp
+(defun str-split (string)
+  (loop for start = 0 then (1+ finish)
+        for finish = (position #\  string :start start)
+        collecting (subseq string start finish)
+        until (null finish)))
+
+(defun get-file (filename)
+  (with-open-file (stream filename)
+                  (loop for line = (read-line stream nil)
+                        while line
+                        collect line)))
