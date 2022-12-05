969a33a74e8d5bb5e07ced23e4fde48dca1c11e4
kristian <kristian@purestyle.se>
2022-12-05 09:45:27 +0100

    cleaning

--- 2022/day5.hs
+++ 2022/day5.hs
@@ -3,27 +3,20 @@ module Day5 where
 import Data.Maybe
 import qualified Data.Map as M
 
-a1 = "TVJWNRMS"
-a2 = "VCPQJDWB"
-a3 = "PRDHFJB"
-a4 = "DNMBPRF"
-a5 = "BTPRVH"
-a6 = "TPBC"
-a7 = "LPRJB"
-a8 = "WBZTLSCN"
-a9 = "GSL"
-
 type Stacks = M.Map Int String
+
 stacks0 :: Stacks
-stacks0 = M.fromList (zip [1..] [a1, a2, a3, a4, a5, a6, a7, a8, a9])
+stacks0 = M.fromList (zip [1..]
+                          ["TVJWNRMS", "VCPQJDWB", "PRDHFJB", "DNMBPRF",
+                           "BTPRVH", "TPBC", "LPRJB", "WBZTLSCN", "GSL"])
 
 main :: IO ()
 main = do
   file <- readFile "input.txt"
-  let l = fmap pline (lines file)
   let acc = M.toList $
-              foldl
-                (\acc instruction -> move acc instruction) stacks0 l
+              foldl (\acc instruction -> move acc instruction)
+                    stacks0
+                    (fmap pline (lines file))
   putStrLn $ "res: " ++ [head x|(_, x)<-acc]
 
 pline :: String -> (Int, Int,Int)
