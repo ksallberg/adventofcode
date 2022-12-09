ac3e3efb0ab4e7c40ab0098e61348dd00b2892e0
Kristian Sallberg <kristian@purestyle.se>
2022-12-09 23:13:04 +0100

    day9 pt2

--- 2022/day9.hs
+++ 2022/day9.hs
@@ -3,45 +3,49 @@ module Day9 where
 import Data.List
 
 type Point = (Int, Int)
-
-data Action = U | D | L | R
-  deriving (Show)
+type Action = Char
 
 main :: IO ()
 main = do
   file <- readFile "input.txt"
   let acts = concat $ fmap parseline (lines file)
-  let (finalHd, finalTl, tailTrail) = logic (1000,1000) (1000,1000) acts
-  putStrLn $ (show (length $ nub tailTrail))
+  let (final, tailTrail) = logic (take 10 (repeat (1000,1000))) acts
+  putStrLn $ (show (length $ nub (reverse tailTrail)))
 
 parseline :: String -> [Action]
-parseline (inst:' ':rest) = take (read rest::Int) (repeat (instToAction inst))
-  where instToAction 'U' = U
-        instToAction 'D' = D
-        instToAction 'L' = L
-        instToAction 'R' = R
+parseline (inst:' ':rest) = take (read rest::Int) (repeat inst)
 
-logic :: Point -> Point -> [Action] -> (Point, Point, [Point])
-logic hd tl acts = foldl step (hd, tl, []) acts
+logic :: [Point] -> [Action] -> ([Point], [Point])
+logic knots acts = foldl step (knots, []) acts
 
-step :: (Point, Point, [Point]) -> Action -> (Point, Point, [Point])
-step (hd, tl, tlhistory) act = (nextHead, nextTail, (nextTail:tlhistory))
-  where nextHead = newhead hd act
-        nextTail = newtl nextHead tl act
+step :: ([Point], [Point]) -> Action -> ([Point], [Point])
+step (knots, tlhistory) act = (reverse newArr2, ((head newArr2):tlhistory))
+  where nextHead = newhead (head knots) act
+        newArr2 = foldl (\acc part ->
+                           (newtl (head acc) part:acc)) [nextHead] (tail knots)
 
 newhead :: Point -> Action -> Point
-newhead (x, y) U = (x, y-1)
-newhead (x, y) D = (x, y+1)
-newhead (x, y) L = (x-1, y)
-newhead (x, y) R = (x+1, y)
+newhead (x, y) 'U' = (x, y-1)
+newhead (x, y) 'D' = (x, y+1)
+newhead (x, y) 'L' = (x-1, y)
+newhead (x, y) 'R' = (x+1, y)
 
-newtl :: Point -> Point -> Action -> Point
-newtl (x, y) (a, b) act = case xdiff of
-                            2 -> let (a2, b2) = newhead (a,b) act
-                                 in (a2, y)
-                            _ -> case ydiff of
-                                   2 -> let (a2, b2) = newhead (a, b) act
-                                        in (x, b2)
-                                   _ -> (a, b)
+newtl :: Point -> Point -> Point
+newtl (x, y) (a, b) = case (xdiff, ydiff) of
+                        (2,2) -> diag  (x, y) (a, b)
+                        (2,_) -> horiz (x, y) (a, b)
+                        (_,2) -> vert  (x, y) (a, b)
+                        _ -> (a, b)
   where xdiff = abs (x - a)
         ydiff = abs (y - b)
+
+diag (x, y) (a, b) | x > a && y > b = (a+1, b+1)
+                   | x < a && y < b = (a-1, b-1)
+                   | x > a && y < b = (a+1, b-1)
+                   | x < a && y > b = (a-1, b+1)
+
+horiz (x, y) (a, b) | x > a = (a+1, y)
+                    | x < a = (a-1, y)
+
+vert (x, y) (a, b) | y > b = (x, b+1)
+                   | y < b = (x, b-1)
