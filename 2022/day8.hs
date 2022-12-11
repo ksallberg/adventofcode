8a3b3b07f2b19397a2adbb1fa1f5917c5b0db98e
Kristian Sallberg <kristian@purestyle.se>
2022-12-11 21:23:51 +0100

    day8 pt 2

--- 2022/day8.hs
+++ 2022/day8.hs
@@ -13,10 +13,11 @@ main = do
   let oneline = map (digitToInt) ((concat . lines) file)
       arr = listArray ((0,0), (len,len)) oneline
       toQuery = [(x, y) | x <- [1..len-1], y <- [1..len-1]]
-      interior = length $ filter (==True) [queryTree tree arr | tree <- toQuery]
-      exterior = ((len+1) * 4 - 4)
-      sum = interior+exterior
-  putStrLn $ (show sum)
+      max = last $ sort [scenic tree arr | tree <- toQuery]
+      -- interior = length $ filter (==True) [queryTree tree arr | tree <- toQuery]
+      -- exterior = ((len+1) * 4 - 4)
+      -- sum = interior+exterior
+  putStrLn $ (show max)
 
 queryTree :: (Int, Int) -> Array (Int, Int) Int -> Bool
 queryTree (col, row) trees = elem True [curTree > last (sort left),
@@ -32,3 +33,25 @@ queryTree (col, row) trees = elem True [curTree > last (sort left),
                   row1 < row && col1 == col]
         below = [height | ((row1, col1), height) <- assocs trees,
                   row1 > row && col1 == col]
+
+--pt 2
+
+scenic :: (Int, Int) -> Array (Int, Int) Int -> Int
+scenic (col, row) trees = (prune curTree $ reverse above) *
+                          (prune curTree $ below) *
+                          (prune curTree $ reverse left) *
+                          (prune curTree $ right)
+  where curTree = trees ! (row, col)
+        left  = [height | ((row1, col1), height) <- assocs trees,
+                  col1 < col && row1 == row]
+        right = [height | ((row1, col1), height) <- assocs trees,
+                  col1 > col && row1 == row]
+        above = [height | ((row1, col1), height) <- assocs trees,
+                  row1 < row && col1 == col]
+        below = [height | ((row1, col1), height) <- assocs trees,
+                  row1 > row && col1 == col]
+
+prune curH trees = length ((takeWhile (<curH) trees)) + more
+  where more = case dropWhile (<curH) trees of
+                 [] -> 0
+                 _ -> 1
