15cc0e81068b556226dc70ab6516012c5d5f034a
Kristian Sallberg <kristian@purestyle.se>
2022-12-14 21:47:38 +0100

    memory issue :(

--- 2022/day14.hs
+++ 2022/day14.hs
@@ -8,7 +8,8 @@ import Data.List.Split
 type Point = (Int, Int)
 type Map = Array (Int, Int) Char
 
-size = 550
+startx = 300
+size = 700
 
 inp = ["498,4 -> 498,6 -> 496,6",
        "503,4 -> 502,4 -> 502,9 -> 494,9"]
@@ -16,10 +17,11 @@ inp = ["498,4 -> 498,6 -> 496,6",
 main :: IO ()
 main = do
   file <- readFile "input.txt"
-  let arr = listArray ((0,0), (size,size)) (repeat '.')
+  let arr = listArray ((startx,0), (size,size)) (repeat '.')
       parsed = fmap parseLine (lines file)
-      arrRock = foldl (\accMap ln -> rockIt ln accMap) arr parsed
       largestY = head $ reverse $ sort (map snd (concat parsed))
+      flooR = [(startx,largestY+2), (size, largestY+2)]
+      arrRock = foldl (\accMap ln -> rockIt ln accMap) arr (flooR:parsed)
   putStrLn $ "largestY: " ++ (show largestY)
   rounds <- manySand 0 largestY arrRock
   putStrLn $ "rounds" ++ (show rounds)
@@ -28,12 +30,12 @@ parseLine :: String -> [(Int, Int)]
 parseLine st = [(read a, read b)| [a,b] <- prep]
     where prep = fmap (splitOn ",") (splitOn " -> " st)
 
--- printSection :: Map -> IO ()
--- printSection map = do
---   let clip = [(x, y) |y<-[0..9], x<-[494..503]]
---       divide = chunksOf 10 clip
---       strs = [fmap (\(x, y) -> map ! (x,y)) d | d <- divide]
---   forM_ strs putStrLn
+printSection :: Map -> IO ()
+printSection map = do
+  let clip = [(x, y) |y<-[0..29], x<-[484..484+29]]
+      divide = chunksOf 30 clip
+      strs = [fmap (\(x, y) -> map ! (x,y)) d | d <- divide]
+  forM_ strs putStrLn
 
 rockIt :: [(Int,Int)] -> Map -> Map
 rockIt [(_,_)] map = map
@@ -54,9 +56,9 @@ manySand round abyssAt map = do
 
 fillSand :: Int -> Int -> (Int, Int) -> Map -> (Bool, Int, Map)
 fillSand round abyss pos@(x,y) map =
-  case (abyssReached, nextBlocked, downLeftBlocked, downRightBlocked) of
-    (True, _, _, _) ->
-      (True, round, map)
+  case (spawnReached, nextBlocked, downLeftBlocked, downRightBlocked) of
+    (True, True, True, True) ->
+       (True, round, map)
     (_, False, _, _) ->
       fillSand round abyss (x,y+1) map
     (_, _, False, _) ->
@@ -65,7 +67,8 @@ fillSand round abyss pos@(x,y) map =
       fillSand round abyss (x+1,y+1) map
     (_, True, True, True) ->
       (False, round, map // [(pos, 'o')])
-   where abyssReached = y >= abyss
+   where spawnReached = x == 500 && y == 0
+         abyssReached = y >= abyss
          nextType = map ! (x, y+1)
          nextBlocked = isBlocked nextType
          downLeft = map ! (x-1,y+1)
