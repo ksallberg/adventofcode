e14cb8e62d1b84cfc208a05cf6b64ad17727b496
Kristian Sallberg <kristian@purestyle.se>
2022-12-19 12:27:13 +0100

    day18 pt2

--- 2022/day18.hs
+++ 2022/day18.hs
@@ -10,22 +10,44 @@ main :: IO ()
 main = do
   file <- readFile "input.txt"
   let cubes = fmap pline (lines file)
-      setCubes = fromList cubes
-      counted = [countFreeSides p setCubes | p <- cubes]
-  putStrLn $ "pt1: " ++ (show (sum counted))
-  putStrLn $ "pt2: " ++ "hej"
+      rock = fromList cubes
+      air = flood (-30) 30 rock
+  putStrLn $ "pt1: " ++ (show (sum [countFreeSides p rock | p <- cubes]))
+  putStrLn $ "pt2: " ++ (show (sum [countFreeSides2 p air | p <- cubes]))
 
 pline :: String -> Point
 pline x = let [a,b,c] = splitOn "," x
           in (read a, read b, read c)
 
 countFreeSides :: Point -> Set Point -> Int
-countFreeSides (x,y,z) s =
-  6 - (Ls.length $ Ls.filter (==True) [member n s|n <- neighbours])
-  where neighbours = [left, right, below, above, closer, further]
-        left = (x-1,y,z)
+countFreeSides p@(x,y,z) s =
+  6 - (Ls.length $ Ls.filter (==True) [member n s | n <- (getNeighbours p)])
+
+countFreeSides2 :: Point -> Set Point -> Int
+countFreeSides2 p@(x,y,z) airs =
+  (Ls.length $ Ls.filter (==True) [member n airs | n <- (getNeighbours p)])
+
+getNeighbours :: Point -> [Point]
+getNeighbours (x,y,z) = [left, right, below, above, closer, further]
+  where left = (x-1,y,z)
         right = (x+1,y,z)
         below = (x,y+1,z)
         above = (x,y-1,z)
         closer = (x,y,z-1)
         further = (x,y,z+1)
+
+flood :: Int -> Int -> Set Point -> Set Point
+flood minp maxp rocks = flood' minp maxp [(minp, minp, minp)] empty rocks
+
+flood' :: Int -> Int -> [Point] -> Set Point -> Set Point -> Set Point
+flood' minp maxp [] air rock = air
+flood' minp maxp (p:lookAt) air rock =
+  case outOfBounds minp maxp p || member p rock || member p air of
+    True ->
+      flood' minp maxp lookAt air rock
+    False ->
+      flood' minp maxp (lookAt++(getNeighbours p)) (insert p air) rock
+
+outOfBounds :: Int -> Int -> Point -> Bool
+outOfBounds minp maxp (x,y,z) =
+  (x > maxp || x < minp) || (y > maxp || y < minp) || (z > maxp || z < minp)
