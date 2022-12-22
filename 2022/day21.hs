aeb16106296d2e6e2e31b8eef818c53457bf1817
Kristian Sallberg <kristian@purestyle.se>
2022-12-22 10:43:28 +0100

    hm

--- 2022/day21.hs
+++ 2022/day21.hs
@@ -11,8 +11,38 @@ main :: IO ()
 main = do
   file <- readFile "input.txt"
   let parsed = fmap parseLine (lines file)
-      root = getMonkey "root" parsed
-  putStrLn $ "pt1: " ++ (show root)
+      -- root = getMonkey "root" parsed -- pt1
+      -- tests = [getMonkey "root" hooman parsed | hooman <- [5000000..]]
+      -- firstTrue = head $ dropWhile (\(bool, num) -> bool == False) tests
+                             -- 3587647562851
+      hmm = getMonkey "root" 3587647562854 parsed
+      hmm2 = getMonkey "root" 3587647562851 parsed
+  firstTrue <- loop 0 1 parsed
+  putStrLn $ "hmm: " ++ (show hmm)
+  putStrLn $ "hmm2: " ++ (show hmm2)
+  putStrLn $ "pt2: " ++ (show firstTrue)
+
+loop :: Integer -> Integer -> [(String, Monkey)] -> IO Integer
+loop x adder parsed = case getMonkey "root" x parsed of
+           (True, y, m1, m2) -> do
+             putStrLn ("___FOUND THE RIGHT " ++ (show m1) ++ " "++ (show m2))
+             return y
+           (False, _, m1, m2) -> do
+             putStrLn ("___adder" ++ show x ++ ", " ++ show adder ++ ", " ++ (show m1) ++ ", " ++ (show m2))
+             case m1 > m2 of
+               -- talet måste öka
+               True -> do
+                 putStrLn "öka"
+                 let newAdder = adder * 2
+                 loop (x+newAdder) newAdder parsed
+               -- talet måste minska
+               False -> do
+                 putStrLn "minksa"
+                 let newAdder = div adder 2
+                 loop (x-newAdder) newAdder parsed
+
+between :: Integer -> Integer -> Integer
+between a b = div (a + b) 2
 
 parseLine :: String -> (String, Monkey)
 parseLine line = case rest2 of
@@ -29,10 +59,18 @@ parseOp "+" = (+)
 parseOp "-" = (-)
 parseOp "*" = (*)
 
-getMonkey :: String -> [(String, Monkey)] -> Integer
-getMonkey name monkeys = case refMonkey of
-                           Yell num ->
-                             num
-                           Operation m1 m2 fun ->
-                             fun (getMonkey m1 monkeys) (getMonkey m2 monkeys)
+getMonkey :: String -> Integer -> [(String, Monkey)] -> (Bool, Integer, Integer, Integer)
+getMonkey "root" hooman monkeys = (monkey1 == monkey2, hooman, monkey1, monkey2)
+  where Operation m1 m2 _ = fromJust $ lookup "root" monkeys
+        monkey1 = getMonkey' m1 hooman monkeys
+        monkey2 = getMonkey' m2 hooman monkeys
+
+getMonkey' :: String -> Integer -> [(String, Monkey)] -> Integer
+getMonkey' "humn" hooman monkeys = hooman
+getMonkey' name hooman monkeys = case refMonkey of
+                                  Yell num ->
+                                    num
+                                  Operation m1 m2 fun ->
+                                    fun (getMonkey' m1 hooman monkeys)
+                                        (getMonkey' m2 hooman monkeys)
   where refMonkey = fromJust $ lookup name monkeys
